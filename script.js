cd7bbef1109f5d957079606de1bc76d5b6403398
Aaron Abele <aaronabele.coding@gmail.com>
2023-03-09 20:51:52 +0100

    adds vue sol

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* 
 const fastCheck = document.querySelector("#fast");
 const cheapCheck = document.querySelector("#cheap");
 const goodCheck = document.querySelector("#good");
@@ -22,3 +23,30 @@ cheapCheck.addEventListener("change", function () {
 goodCheck.addEventListener("change", function () {
   toggle(goodCheck, cheapCheck, fastCheck);
 });
+*/
+
+Vue.createApp({
+  data() {
+    return {
+      checkboxes: [
+        { id: 1, value: "fast", done: true },
+        { id: 2, value: "cheap", done: false },
+        { id: 3, value: "good", done: false },
+      ],
+      selectedCheckboxes: [],
+    };
+  },
+  watch: {
+    selectedCheckboxes: {
+      handler(newValue, oldValue) {
+        if (newValue.length === 3) {
+          this.selectedCheckboxes.splice(-2, 1);
+        }
+        this.checkboxes.forEach((checkbox) => {
+          checkbox.done = this.selectedCheckboxes.includes(checkbox.value);
+        });
+      },
+      deep: true,
+    },
+  },
+}).mount("#app");
